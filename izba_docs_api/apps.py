ebdf5705530d9d04f368345fcd97519b2e456a8a
Przemek Kaliś <przemek.kalis@gmail.com>
2021-07-21 06:28:32 +0000

    add verbose name

--- izba_docs_api/apps.py
+++ izba_docs_api/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class IzbaDocsApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "izba_docs_api"
+    verbose_name = "Izba Docs API"
 
     def ready(self):
         from . import signals

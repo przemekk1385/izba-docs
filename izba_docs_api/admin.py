3bcbe1bfa3c30707ec345d4e01b5d072327084c0
Przemek Kaliś <przemek.kalis@gmail.com>
2021-07-21 06:29:09 +0000

    enhance event form

--- izba_docs_api/admin.py
+++ izba_docs_api/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from . import models
 
+
+class InlineDocument(admin.StackedInline):
+
+    extra = 1
+    model = models.Document
+
+
+@admin.register(models.Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [InlineDocument]
+
+
 admin.site.register(models.Document)
-admin.site.register(models.Event)
 admin.site.register(models.Tag)

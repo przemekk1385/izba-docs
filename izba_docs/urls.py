543fed4966d05ab6ff19ee81c6db7976713e8763
Przemek Kaliś <przemek.kalis@gmail.com>
2021-07-27 20:01:12 +0000

    add static and auth paths

--- izba_docs/urls.py
+++ izba_docs/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("izba_docs_api.urls")),
-]
+    path("api-token-auth/", views.obtain_auth_token),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
